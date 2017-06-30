8460fd5f851275bf7bcf19f9f4019aedf520feed
VahanAper <asryanvahandev@gmail.com>
2017-06-30 15:58:29 +0400

    get address from cli

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    address: {
+      demand: true,
+      describe: 'Address to fetch wehater for.',
+      alias: 'a',
+      string: true,
+    },
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelpia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true,
 }, (error, response, body) => {
   const { results: { 0: result } } = body;
+
   console.log(`Adress: ${result.formatted_address}`);
   console.log(`Latitude: ${result.geometry.location.lat}`);
   console.log(`Longitude: ${result.geometry.location.lng}`);
